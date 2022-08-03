c595a3c77bd8c7926e6f22fbd6bb7c8c26874016
dii brothimer <Dushime Brother>
2022-08-03 18:06:03 +0100

    first commit

---
+++ myportifolio.js
@@ -0,0 +1,6 @@
+function displayName(){
+    document.getElementById("my-name").style.cssText ="display: initial";
+}
+function nonName(){
+    document.getElementById("my-name").style.cssText ="display: none"
+}
